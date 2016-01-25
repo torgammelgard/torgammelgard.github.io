155a9c1a8c89f0ed5426b2a55f911a9fbaec3e47
Tor Gammelgård <tor.gammelgard@gmail.com>
2016-01-25 16:25:01 +0100

    Some improvments to style and additions to content

--- FrontEndProject/main_script.js
+++ FrontEndProject/main_script.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-   $('#footer').on('mouseover', function() {
-     $(this).stop();
-     $(this).animate({height: '125px', opacity: 1}, {duration: 800});
-   });
-   $('#footer').on('mouseleave', function() {
-     $(this).stop();
-     $(this).animate({height: '25px', opacity: 0.5}, {duration: 800});
-   });
+  init_footer();
+  $('#footer').on('mouseover', function() {
+    $(this).stop();
+    $(this).animate({height: '125px', opacity: 1}, {duration: 800});
+  });
+  $('#footer').on('mouseleave', function() {
+    $(this).stop();
+    $(this).animate({height: '25px', opacity: 0.5}, {duration: 800});
+  });
+  $('nav').find('li').hover(function() {
+    $(this).closest('ul').toggleClass('color-nav-hover');
+  });
 });
+
+function init_footer() {
+  $('footer').animate({height: '25px'}, {duration: 1000});
+}
